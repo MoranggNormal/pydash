32a5942ab6e5af426c68ef9d649e9e2d8c9fa0e8
Euller Peixoto <epeixoto.dev@gmail.com>
2024-01-20 08:56:27 -0300

    #1 create figure annotation handler

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import plotly.graph_objects as go
+
 def parse_data(data):
     month_mapping = {'Jan': 1, 'Fev': 2, 'Mar': 3, 'Abr': 4,
                  'Mai': 5, 'Jun': 6, 'Jul': 7, 'Ago': 8,
@@ -20,3 +22,19 @@ def parse_data(data):
 
 def graph_title(title):
     return dict(text=f"{title}", font=dict(size=24))
+
+def create_average_annotation(df, text):
+
+    annotation = go.layout.Annotation(
+        text=text,
+        xref="paper",
+        yref="paper",
+        font=dict(size=30, color='gray'),
+        align="center",
+        bgcolor="rgba(0,0,0,0.8)",
+        x=0.025,
+        y=0.90,
+        showarrow=False
+    )
+
+    return annotation
