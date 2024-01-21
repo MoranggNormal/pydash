ccf471d294473a80acd58ab216ced104f8b78948
Euller Peixoto <epeixoto.dev@gmail.com>
2024-01-21 12:43:17 -0300

    #8 update data by selected team

--- app.py
+++ app.py
@@ -37,8 +37,21 @@ parsed_data = parse_data(df)
 
 app = Dash(__name__)
 
+team_options = [{'label': 'Todas as Equipes', 'value': 'all_teams'}]
+for team in parsed_data['Equipe'].unique():
+    team_options.append({'label': team, 'value': team})
+
 app.layout = html.Div([
     html.H1(children='Python Dash', style={'textAlign':'center'}),
+    
+    html.Div([
+        dcc.Dropdown(
+            options=team_options,
+            value='all_teams',
+            clearable=False,
+            id='current_team'
+        )]),
+    
     dcc.Graph(id='team_sales'),
     dcc.Graph(id='average_calls_by_days_of_the_month'),
     dcc.Graph(id='average_calls_by_month'),
@@ -60,24 +73,52 @@ def update_graph_team_sales(clickAnnotationData):
     data = parsed_data.groupby('Equipe')['Valor Pago'].sum().reset_index()
     return graph_team_sales(data)
 
-@callback(Output('average_calls_by_days_of_the_month', 'figure'), Input('average_calls_by_days_of_the_month', 'clickAnnotationData'))
-def update_graph_average_calls_by_days_of_the_month(clickAnnotationData):
-    data = parsed_data.groupby('Dia')['Chamadas Realizadas'].sum().reset_index()
+@callback(
+        Output('average_calls_by_days_of_the_month', 'figure'), 
+        Input('average_calls_by_days_of_the_month', 'clickAnnotationData'),
+        Input('current_team', 'value'))
+def update_graph_average_calls_by_days_of_the_month(clickAnnotationData, value):
+    if value == 'all_teams':
+        data = parsed_data.groupby('Dia')['Chamadas Realizadas'].sum().reset_index()
+    else:
+        data = parsed_data[parsed_data['Equipe'] == value]
+        data = data.groupby('Dia')['Chamadas Realizadas'].sum().reset_index()
     return graph_average_calls_by_days_of_the_month(data)
 
-@callback(Output('average_calls_by_month', 'figure'), Input('average_calls_by_month', 'clickAnnotationData'))
-def update_graph_average_calls_by_month(clickAnnotationData):
-    data = parsed_data.groupby('Mês')['Chamadas Realizadas'].sum().reset_index()
+@callback(
+        Output('average_calls_by_month', 'figure'), 
+        Input('average_calls_by_month', 'clickAnnotationData'),
+        Input('current_team', 'value'))
+def update_graph_average_calls_by_month(clickAnnotationData, value):
+    if value == 'all_teams':
+        data = parsed_data.groupby('Mês')['Chamadas Realizadas'].sum().reset_index()
+    else:
+        data = parsed_data[parsed_data['Equipe'] == value]
+        data = data.groupby('Mês')['Chamadas Realizadas'].sum().reset_index()
     return graph_average_calls_by_month(data)
 
-@callback(Output('amounts_paid_through_advertising', 'figure'), Input('amounts_paid_through_advertising', 'clickAnnotationData'))
-def update_graph_amounts_paid_through_advertising(clickAnnotationData):
-    data = parsed_data.groupby(['Meio de Propaganda', 'Mês'])['Valor Pago'].sum().reset_index()
+@callback(
+    Output('amounts_paid_through_advertising', 'figure'),
+    Input('amounts_paid_through_advertising', 'hoverData'),
+    Input('current_team', 'value'))
+def update_graph_amounts_paid_through_advertising(hoverData, value):
+    if value == 'all_teams':
+        data = parsed_data.groupby(['Meio de Propaganda', 'Mês'])['Valor Pago'].sum().reset_index()
+    else:
+        data = parsed_data[parsed_data['Equipe'] == value]
+        data = data.groupby(['Meio de Propaganda', 'Mês'])['Valor Pago'].sum().reset_index()
     return graph_amounts_paid_through_advertising(data)
 
-@callback(Output('advertising_on_piechart', 'figure'), Input('advertising_on_piechart', 'clickAnnotationData'))
-def update_graph_advertising_on_piechart(clickAnnotationData):
-    data = parsed_data.groupby('Meio de Propaganda')['Valor Pago'].sum().reset_index()
+@callback(
+    Output('advertising_on_piechart', 'figure'),
+    Input('advertising_on_piechart', 'clickAnnotationData'),
+    Input('current_team', 'value'))
+def update_graph_advertising_on_piechart(clickAnnotationData, value):
+    if value == 'all_teams':
+        data = parsed_data.groupby('Meio de Propaganda')['Valor Pago'].sum().reset_index()
+    else:
+        data = parsed_data[parsed_data['Equipe'] == value]
+        data = data.groupby('Meio de Propaganda')['Valor Pago'].sum().reset_index()
     return graph_advertising_on_piechart(data)
 
 @callback(Output('earnings_per_month_plus_segregation_by_team', 'figure'), Input('earnings_per_month_plus_segregation_by_team', 'clickAnnotationData'))
@@ -105,9 +146,16 @@ def update_graph_indicators_best_equip(clickAnnotationData):
     data = data.reset_index()
     return graph_indicators_best_equip(data)
 
-@callback(Output('indicators_total_earnings', 'figure'), Input('indicators_total_earnings', 'clickAnnotationData'))
-def update_graph_indicators_total_earnings(clickAnnotationData):
-    return graph_indicators_total_earnings(parsed_data)
+@callback(
+    Output('indicators_total_earnings', 'figure'), 
+    Input('indicators_total_earnings', 'clickAnnotationData'),
+    Input('current_team', 'value'))
+def update_graph_indicators_total_earnings(clickAnnotationData, value):
+    if value == 'all_teams':
+        data = parsed_data
+    else:
+        data = parsed_data[parsed_data['Equipe'] == value]
+    return graph_indicators_total_earnings(data)
 
 @callback(Output('indicators_total_calls', 'figure'), Input('indicators_total_calls', 'clickAnnotationData'))
 def update_graph_indicators_total_calls(clickAnnotationData):
